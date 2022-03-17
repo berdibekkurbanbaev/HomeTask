2b835ff3c7b734c0f5edda6e8b10b555d0e69b9b
Berdibek Kurbanbaev <berdibekkurbanbaev@MacBook-Air.local>
2022-03-18 00:00:04 +0300

    Initial commit

---
+++ src/com/company/Class1.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Class1 {
+    private int number;
+    private String word;
+    private int [] massiv;
+
+    public Class1(int number, String word, int[] massiv) {
+        this.number = number;
+        this.word = word;
+        this.massiv = massiv;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String [] getMassiv() {
+        return new String[]{Arrays.toString(massiv)};
+    }
+
+    public void setMassiv(int[] massiv) {
+        this.massiv = massiv;
+    }
+
+    @Override
+    public String toString() {
+        return "Class1" +
+                " number = " + number +
+                ", word = " + word  +
+                "  massiv = " + Arrays.toString(massiv) +""
+                ;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Class1 class1 = new Class1(1,"Hello World!",new int[]{2,3,4,5,2});
+
+        System.out.println(class1);
+    }
+}
